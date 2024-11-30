91adac9558115f9d64c13477a607341970158a84
Brooke Chalmers <breq@breq.dev>
2024-11-30 16:38:00 -0500

    change canonical time display

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Generator, generators } from "./data";
+import { useEffect, useState } from "react";
+import { ClockTime, Generator, generators } from "./data";
 
-function timeUntilWallClock(targetHour: number, targetMinute: number) {
+function timeUntilWallClock(time: ClockTime) {
   const now = new Date();
   // Construct a new date object with the next occurrence of the target time
   const targetDate = new Date(now);
 
-  targetDate.setHours(targetHour);
-  targetDate.setMinutes(targetMinute);
+  targetDate.setHours(time.hours);
+  targetDate.setMinutes(time.minutes);
   targetDate.setSeconds(59);
   targetDate.setMilliseconds(999);
 
@@ -18,19 +19,9 @@ function timeUntilWallClock(targetHour: number, targetMinute: number) {
 }
 
 function timeUntilGenerator(generator: Generator) {
-  if (generator.clock === "12") {
-    const morning = timeUntilWallClock(
-      generator.localHours,
-      generator.localMinutes
-    );
-    const evening = timeUntilWallClock(
-      generator.localHours + 12,
-      generator.localMinutes
-    );
-    return Math.min(morning, evening);
-  } else {
-    return timeUntilWallClock(generator.localHours, generator.localMinutes);
-  }
+  return generator.clockTimes
+    .map((clockTime) => timeUntilWallClock(clockTime))
+    .reduce((prev, curr) => Math.min(prev, curr));
 }
 
 function GeneratorView({ generator }: { generator: Generator }) {
@@ -56,8 +47,7 @@ function GeneratorView({ generator }: { generator: Generator }) {
         <div className="inline-flex justify-between w-[calc(100%-22px)]">
           <h2 className="">{generator.name}</h2>
           <span className="">
-            {generator.localHours}:
-            {String(generator.localMinutes).padStart(2, "0")}{" "}
+            {generator.canonicalTime}{" "}
             <span
               className={
                 state === "now"
@@ -78,7 +68,18 @@ function GeneratorView({ generator }: { generator: Generator }) {
   );
 }
 
+function useDate() {
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    const interval = setInterval(() => setDate(new Date()), 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return date;
+}
+
 export default function App() {
+  const now = useDate();
+
   return (
     <div className="flex flex-col p-4 gap-4 font-mono">
       <div>
@@ -86,6 +87,13 @@ export default function App() {
       </div>
 
       <div className="flex flex-col w-full gap-2 max-w-2xl">
+        <div className="flex flex-row text-2xl justify-between pl-4 pr-[calc(0.75rem+2px)]">
+          <span>the time is...</span>
+          <span className="mr-[9ch]">
+            {String(now.getHours()).padStart(2, "0")}:
+            {String(now.getMinutes()).padStart(2, "0")}
+          </span>
+        </div>
         {generators
           .sort((a, b) => timeUntilGenerator(a) - timeUntilGenerator(b))
           .map((generator) => (
--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,15 @@
+export type ClockTime = {
+  hours: number;
+  minutes: number;
+};
+
 export type Generator = {
   url: string;
   name: string;
   description: string;
   protocol: string;
-  localHours: number;
-  localMinutes: number;
-  clock: "1" | "12" | "24";
+  clockTimes: ClockTime[];
+  canonicalTime: string;
 };
 
 export const generators: Generator[] = [
@@ -15,36 +19,41 @@ export const generators: Generator[] = [
     description:
       "a random fish generator that only works at 11:11 each morning and evening, in your local timezone.",
     protocol: "http",
-    localHours: 11,
-    localMinutes: 11,
-    clock: "12",
+    clockTimes: [
+      { hours: 11, minutes: 11 },
+      { hours: 23, minutes: 11 },
+    ],
+    canonicalTime: "11:11",
   },
   {
     url: "https://fish.lftq.in/",
     name: "fish.lftq.in",
     description: "minecraft-inspired 3D fish",
     protocol: "http",
-    localHours: 11,
-    localMinutes: 11,
-    clock: "12",
+    clockTimes: [
+      { hours: 11, minutes: 11 },
+      { hours: 23, minutes: 11 },
+    ],
+    canonicalTime: "11:11",
   },
   {
     url: "https://tris.fyi/dish",
     name: "bake a dish",
     description: "a random allrecipies generator that only works as 22:22",
     protocol: "http",
-    localHours: 22,
-    localMinutes: 22,
-    clock: "24",
+    clockTimes: [{ hours: 22, minutes: 22 }],
+    canonicalTime: "22:22",
   },
   {
     url: "https://fissh.breq.dev/",
     name: "ssh a fissh",
     description: "a random ASCII fish generator that works over SSH",
     protocol: "ssh",
-    localHours: 11,
-    localMinutes: 11,
-    clock: "12",
+    clockTimes: [
+      { hours: 11, minutes: 11 },
+      { hours: 23, minutes: 11 },
+    ],
+    canonicalTime: "11:11",
   },
   {
     url: "https://miakizz.quest/xfish",
@@ -52,17 +61,32 @@ export const generators: Generator[] = [
     description:
       "a random fish generator that connects to your X11 server to draw a fish",
     protocol: "x11",
-    localHours: 11,
-    localMinutes: 11,
-    clock: "12",
+    clockTimes: [
+      { hours: 11, minutes: 11 },
+      { hours: 23, minutes: 11 },
+    ],
+    canonicalTime: "11:11",
   },
   {
     url: "https://makeabyte.lftq.in/",
     name: "2:55 make a byte",
     description: "random byte generator that shows the ASCII, bin, and hex",
     protocol: "http",
-    localHours: 2,
-    localMinutes: 55,
-    clock: "12",
+    clockTimes: [
+      { hours: 2, minutes: 55 },
+      { hours: 14, minutes: 55 },
+    ],
+    canonicalTime: "02:55",
+  },
+  {
+    url: "https://queercomputerclub.ca/projects/quecey-voip/",
+    name: "dial-a-fish",
+    description: "SSTV-encoded fish delivered over a telephone call",
+    protocol: "tel",
+    clockTimes: Array.from({ length: 23 }, (_, i) => ({
+      hours: i,
+      minutes: 11,
+    })),
+    canonicalTime: "**:11",
   },
 ];
